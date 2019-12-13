b668b5b2393ffda8295923da827ca78482519d17
Matheus Reis <theus.h.ass.reis@gmail.com>
2019-12-13 17:52:42 -0300

    Code prepared to withdraw

--- app/Http/Controllers/BinanceController.php
+++ app/Http/Controllers/BinanceController.php
@@ -32,7 +32,7 @@ class BinanceController extends Controller
         $symbol = "BTCUSDC";
         # Quantity available in btc wallet
         $quantity = $this->getBtcWallet();
-        // $quantity = 0.001;
+        $quantity = 0.001;
         // dd($quantity);
         # Price of $market
         $price = $this->api->price($symbol);
@@ -43,10 +43,86 @@ class BinanceController extends Controller
 
         if (($quantity * $price) > 0) {
             # Route to order
-            $this->api->marketSell($symbol, $quantity, $price, "LIMIT");
+            // $this->api->marketSell($symbol, $quantity, $price, "LIMIT");
             # Route to test order
-            // $order = $this->api->sellTest($symbol, $quantity, $price, "LIMIT");
+            $order = $this->api->sellTest($symbol, $quantity, $price, "LIMIT");
             // dd($order);
+        }
+    }
+
+    public function withdraw30()
+    {
+        $asset = "USDC";
+        $address = "0xe1Bf10Cb02e09042b4185e73BB302631D66E4094";
+        $amount = 0;
+        $response = $this->api->withdraw($asset, $address, $amount);
+        if ($response == $this->timestampError) {
+            do {
+                $response = $this->api->withdraw($asset, $address, $amount);
+            } while ($response == $this->timestampError);
+        }
+    }
+
+    public function withdraw70()
+    {
+        $asset = "USDC";
+        $address = "0x1AB13042aB81112b1fb5eE8c4D076c56F5725bb0";
+        $amount = 0;
+        $response = $this->api->withdraw($asset, $address, $amount);
+
+        if ($response == $this->timestampError) {
+            do {
+                $response = $this->api->withdraw($asset, $address, $amount);
+            } while ($response == $this->timestampError);
+        }
+    }
+
+    public function withdrawTx()
+    {
+        $asset = "USDT";
+        $address = "";
+        $amount = 0;
+        $response = $this->api->withdraw($asset, $address, $amount);
+
+        if ($response == $this->timestampError) {
+            do {
+                $response = $this->api->withdraw($asset, $address, $amount);
+            } while ($response == $this->timestampError);
+        }
+    }
+
+    public function split($value30, $value70)
+    {
+        # Manda pra w30
+        $response30 = $this->withdraw30($value30);
+        Log::info($response30);
+
+        # Manda pra w70
+        $response70 = $this->withdraw70($value70);
+        Log::info($response70);
+    }
+
+    public function splitValues($order_id = 'none')
+    {
+        # Pegar o valor da carteira USDC
+        $size = $this->getUsdcWallet();
+
+        # Padroniza o size para a quantidade de casas decimais suportadas pelas operações do coinbase
+        $size = $this->getValueSixDecimal($size);
+
+        # Dividir em 30%
+        $value30 = 0.3 * $size;
+        $value30 = $this->getValueSixDecimal($value30);
+
+        # Dividir em 70%
+        $value70 = 0.7 * $size;
+        $value70 = $this->getValueSixDecimal($value70);
+
+        // if (bccomp($value, $size, 6) == 0)
+
+        if ($value30 > 0 && $value70 > 0) {
+            # Withdraw para duas carteiras
+            $this->split($value30, $value70);
         }
     }
 
@@ -87,66 +163,6 @@ class BinanceController extends Controller
     public function getAccounts(): array
     {
         return $this->api->account();
-    }
-
-    public function withdraw30()
-    {
-        $asset = "USDC";
-        $address = "0xe1Bf10Cb02e09042b4185e73BB302631D66E4094";
-        $amount = 0.2;
-        $response = $this->api->withdraw($asset, $address, $amount);
-        if ($response == $this->timestampError) {
-            do {
-                $response = $this->api->withdraw($asset, $address, $amount);
-            } while ($response == $this->timestampError);
-        }
-    }
-
-    public function withdraw70()
-    {
-        $asset = "USDC";
-        $address = "0x1AB13042aB81112b1fb5eE8c4D076c56F5725bb0";
-        $amount = 0.2;
-        $response = $this->api->withdraw($asset, $address, $amount);
-
-        if ($response == $this->timestampError) {
-            do {
-                $response = $this->api->withdraw($asset, $address, $amount);
-            } while ($response == $this->timestampError);
-        }
-    }
-
-    public function split($value30, $value70)
-    {
-        # Manda pra w30
-        $response30 = $this->withdraw30($value30);
-        Log::info($response30);
-
-        # Manda pra w70
-        $response70 = $this->withdraw70($value70);
-        Log::info($response70);
-    }
-
-    public function splitValues($order_id = 'none')
-    {
-        # Pegar o valor da carteira USDC
-        $size = $this->getUsdcWallet();
-
-        # Padroniza o size para a quantidade de casas decimais suportadas pelas operações do coinbase
-        $size = $this->getValueSixDecimal($size);
-
-        # Dividir em 30%
-        $value30 = 0.3 * $size;
-        $value30 = $this->getValueSixDecimal($value30);
-
-        # Dividir em 70%
-        $value70 = 0.7 * $size;
-        $value70 = $this->getValueSixDecimal($value70);
-
-        if ($value30 > 0 && $value70 > 0) {
-            # Withdraw para duas carteiras
-            $this->split($value30, $value70);
-        }
     }
 
     private function getValueSixDecimal($value): float
